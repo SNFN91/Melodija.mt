b78567296e2832eeb9f3f37f4d8c447acb5540fb
Shaun Falzon <snfn91contact@gmail.com>
2025-04-23 22:35:45 +0200

    Force rebuild for updated Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Header />
-            <main className="flex-grow">
-              <Home />
-            </main>
-            <Footer />
-          </>
-        }
-      />
-    </Routes>
+    <>
+      <Home />
+    </>
   );
 }
 
