9ac2667b406186a4f1ffe1edce5357f2d6c52793
Shaun Falzon <snfn91contact@gmail.com>
2025-04-23 21:59:25 +0200

    Final fix: homepage now loads directly at root

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import SplashScreen from "./pages/SplashScreen";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SplashScreen />} />
       <Route
-        path="/home"
+        path="/"
         element={
           <>
             <Header />
